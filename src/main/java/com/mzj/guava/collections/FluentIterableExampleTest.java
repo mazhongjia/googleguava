9b658a105b33778d2e3b89c017b76f37919ca1ce
mazhongjia <mazhongjia126@126.com>
2020-03-01 19:57:08 +0800

    20200301

--- src/main/java/com/mzj/guava/collections/FluentIterableExampleTest.java
+++ src/main/java/com/mzj/guava/collections/FluentIterableExampleTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -22,5 +23,21 @@ public class FluentIterableExampleTest {
         FluentIterable<String> listsResult = lists.filter(e->e!= null && e.length()>1);
         System.out.println(listsResult);
         assertThat(listsResult.size(),equalTo(2));
+    }
+
+    @Test
+    public void testCycle(){
+        FluentIterable<String> lists = build();
+        FluentIterable<String> cycle = lists.cycle().limit(20);//cycle是循环，相当于JAVA8的无限流generate
+        System.out.println(cycle);
+    }
+
+    @Test
+    public void testTransformAndConcat(){
+        FluentIterable<String> fit = build();
+        List<Integer> list = Lists.newArrayList(1,2);
+
+        FluentIterable<Integer> result = fit.transformAndConcat(e ->list);//等价于java8的flatMap效果
+        System.out.println(result);
     }
 }
